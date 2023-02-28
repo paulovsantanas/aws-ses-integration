f67e2402d2f1ecfabda7f57e4c01d10b4a01ff4f
Paulo Vitor <paulo.silva@zgsolucoes.com.br>
2023-02-28 12:27:24 -0300

    feat: changing the gradle project to multi-project layout, including the library and the application

---
+++ lib/src/main/java/com/example/aws/sesintegration/SesintegrationLibrary.java
@@ -0,0 +1,9 @@
+package com.example.aws.sesintegration;
+
+public class SesintegrationLibrary {
+
+	public boolean someLibraryMethod() {
+		return true;
+	}
+
+}
---
+++ lib/src/test/java/com/example/aws/sesintegration/SesintegrationLibraryTest.java
@@ -0,0 +1,15 @@
+package com.example.aws.sesintegration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SesintegrationLibraryTest {
+
+	@Test
+	void someLibraryMethodReturnsTrue() {
+		SesintegrationLibrary classUnderTest = new SesintegrationLibrary();
+		assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+	}
+
+}
