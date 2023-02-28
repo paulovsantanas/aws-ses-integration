a5da42cb099c4b90fe32d9ed8563bf39df19c32c
Paulo Vitor <paulo.silva@zgsolucoes.com.br>
2023-02-28 11:26:30 -0300

    feat: Commit Inicial

---
+++ src/main/java/com/example/aws/sesintegration/SesintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws.sesintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesintegrationApplication.class, args);
+	}
+
+}
