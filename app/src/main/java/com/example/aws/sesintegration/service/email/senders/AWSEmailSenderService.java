6c33dc291cd59a2853e38adcfd8962c540970008
Paulo Vitor <paulo.silva@zgsolucoes.com.br>
2023-03-01 10:19:06 -0300

    refactor(AWS SES): Extraindo código para métodos builders

--- app/src/main/java/com/example/aws/sesintegration/service/email/senders/AWSEmailSenderService.java
+++ app/src/main/java/com/example/aws/sesintegration/service/email/senders/AWSEmailSenderService.java
@@ -11,44 +11,25 @@ import java.util.List;
 @Service
 public class AWSEmailSenderService implements IEmailSender {
 
-	@Override
-	public void send(String sender, List<String> recipients, String subject, String bodyHTML) {
-		Region region = Region.US_EAST_1;
-		SesV2Client client = SesV2Client.builder()
-				.region(region)
+	SesV2Client client;
+
+	public AWSEmailSenderService() {
+		this.client = SesV2Client.builder()
+				.region(Region.US_EAST_1)
 				.credentialsProvider(ProfileCredentialsProvider.create())
 				.build();
+	}
 
-		Destination destination = Destination.builder()
-				.toAddresses(recipients)
-				.build();
+	@Override
+	public void send(final String sender, final List<String> recipients, final String subject, final String bodyHTML) {
+		Destination destination = this.buildDestination(recipients);
 
-		Content content = Content.builder()
-				.data(bodyHTML)
-				.build();
+		Content sub = this.buildContent(subject);
+		Body body = this.buildBody(bodyHTML);
 
-		Content sub = Content.builder()
-				.data(subject)
-				.build();
+		EmailContent emailContent = this.buildEmailContent(sub, body);
 
-		Body body = Body.builder()
-				.html(content)
-				.build();
-
-		Message msg = Message.builder()
-				.subject(sub)
-				.body(body)
-				.build();
-
-		EmailContent emailContent = EmailContent.builder()
-				.simple(msg)
-				.build();
-
-		SendEmailRequest emailRequest = SendEmailRequest.builder()
-				.destination(destination)
-				.content(emailContent)
-				.fromEmailAddress(sender)
-				.build();
+		SendEmailRequest emailRequest = this.buildSendEmailRequest(destination, emailContent, sender);
 
 		try {
 			System.out.println("Attempting to send an email through Amazon SES " + "using the AWS SDK for Java...");
@@ -58,6 +39,47 @@ public class AWSEmailSenderService implements IEmailSender {
 		} catch (SesV2Exception e) {
 			System.err.println(e.awsErrorDetails().errorMessage());
 		}
+	}
+
+	private Destination buildDestination(final List<String> recipients) {
+		return Destination.builder()
+				.toAddresses(recipients)
+				.build();
+	}
+
+	private Content buildContent(final String data) {
+		return Content.builder()
+				.data(data)
+				.build();
+	}
+
+	private Body buildBody(final String bodyHTML) {
+		Content content = this.buildContent(bodyHTML);
+		return Body.builder()
+				.html(content)
+				.build();
+	}
+
+	private Message buildMessage(final Content subject, final Body body) {
+		return Message.builder()
+				.subject(subject)
+				.body(body)
+				.build();
+	}
+
+	private EmailContent buildEmailContent(final Content subject, final Body body) {
+		Message message = this.buildMessage(subject, body);
+		return EmailContent.builder()
+				.simple(message)
+				.build();
+	}
+
+	private SendEmailRequest buildSendEmailRequest(final Destination destination, final EmailContent emailContent, final String sender) {
+		return SendEmailRequest.builder()
+				.destination(destination)
+				.content(emailContent)
+				.fromEmailAddress(sender)
+				.build();
 	}
 
 }
